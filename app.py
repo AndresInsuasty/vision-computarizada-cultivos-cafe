a887e811c1e4d42227e86dae1b48d9e2a6ed3882
Andres <andresinsuastyd10@gmail.com>
2021-11-10 19:50:04 -0500

    seleccionado deteccion o segmentacion

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 # CODIGO
-st.title('Sistema de Visión Computarizada con Inteligencia Artificial para Extraer Información de Cultivos de Café en el Sur Occidente Colombiano')
-st.text('Creado por: Andres Insuasty')
-st.image('imgs/Generador U-Net.png')
-uploaded_file = st.file_uploader('Subir Imagen',accept_multiple_files=False,type="jpg")
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=False,width=200)
+st.image('imgs/IntroTesisColor Cafe.png')
+# uploaded_file = st.file_uploader('Subir Imagen',accept_multiple_files=False,type="jpg")
+# if uploaded_file is not None:
+#     image = Image.open(uploaded_file)
+#     st.image(image, caption='Uploaded Image.', use_column_width=False,width=200)
 
-## TODO
-# crear un boton para seleccionar deteccion de objetos y segmentacion de objetos
+selection = st.radio('Escoger uno', ['Segmentación', 'Detección'])
+if selection == 'Segmentación':
+    st.write('Usted escogió segmentación')
+if selection == 'Detección':
+    st.write('Usted escogió detección')
